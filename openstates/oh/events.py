a4b40c9377eff63e3c6df22a33b2106201c491ed
Tim Showers <showerst@gmail.com>
2017-06-09 14:35:44 -0400

    import OH

--- openstates/oh/events.py
+++ openstates/oh/events.py
@@ -4,39 +4,36 @@ import re
 
 import pytz
 
-from pupa.scrape import Scraper, Event
-from pupa.utils.generic import convert_pdf
+from billy.scrape.events import EventScraper, Event
+from billy.scrape.utils import convert_pdf
 
 
-class OHEventScraper(Scraper):
+class OHEventScraper(EventScraper):
     jurisdiction = 'oh'
     _tz = pytz.timezone('US/Eastern')
 
-    def scrape(self, chamber=None):
+    def scrape(self, chamber, session):
         if chamber == "lower":
-            yield from self.scrape_lower()
+            self.scrape_lower(session)
         elif chamber == 'upper':
-            yield from self.scrape_upper()
-        else:
-            yield from self.scrape_lower()
-            yield from self.scrape_upper()
+            self.scrape_upper(session)
 
-    def scrape_lower(self):
+    def scrape_lower(self, session):
         PDF_URL = 'http://www.ohiohouse.gov/Assets/CommitteeSchedule/calendar.pdf'
         (path, _response) = self.urlretrieve(PDF_URL)
-        text = convert_pdf(path, type='text-nolayout').decode()
+        text = convert_pdf(path, type='text')
         os.remove(path)
 
-        days = re.split(r'(\wF+day, \w+ \d{1,2}, 20\d{2})', text)
+        days = re.split(r'(\w+day, \w+ \d{1,2}, 20\d{2})', text)
         date = None
-        for day in enumerate(days[1:]):
+        for day in enumerate(days[1: ]):
             if day[0] % 2 == 0:
                 date = day[1]
             else:
 
                 events = re.split(r'\n((?:\w+\s?)+)\n', day[1])
                 comm = ''
-                for event in enumerate(events[1:]):
+                for event in enumerate(events[1: ]):
                     if event[0] % 2 == 0:
                         comm = event[1].strip()
                     else:
@@ -67,41 +64,49 @@ class OHEventScraper(Scraper):
                                 x.strip() for x in
                                 description.split('\n')
                                 if x.strip() and not x.strip()[0].isdigit()
-                                ])
+                                ]).decode('ascii', 'ignore')
 
                         if not description:
                             description = '[No description provided by state]'
 
                         event = Event(
-                                name=description,
-                                start_time=time,
-                                location_name=location,
-                                timezone=self._tz.zone,
-                                description=description
-                        )
+                                session=session,
+                                when=time,
+                                type='committee:meeting',
+                                description=description,
+                                location=location
+                                )
+
                         event.add_source(PDF_URL)
-                        event.add_participant(comm, type='committee', note='host')
+                        event.add_participant(
+                                type='host',
+                                participant=comm,
+                                participant_type='committee',
+                                chamber='lower'
+                                )
                         for line in description.split('\n'):
-                            related_bill = re.search(r'(H\.?(?:[JC]\.?)?[BR]\.?\s+\d+)\s+(.*)$',
-                                                     line)
+                            related_bill = re.search(r'(H\.?(?:[JC]\.?)?[BR]\.?\s+\d+)\s+(.*)$', line)
                             if related_bill:
                                 (related_bill, relation) = related_bill.groups()
                                 relation = relation.strip()
                                 related_bill = related_bill.replace(".", "")
-                                item = event.add_agenda_item(relation)
-                                item.add_bill(related_bill)
+                                event.add_related_bill(
+                                        bill_id=related_bill,
+                                        type='consideration',
+                                        description=relation
+                                        )
 
-                        yield event
+                        self.save_event(event)
 
-    def scrape_upper(self):
+    def scrape_upper(self, session):
         PDF_URL = 'http://www.ohiosenate.gov/Assets/CommitteeSchedule/calendar.pdf'
         (path, _response) = self.urlretrieve(PDF_URL)
-        text = convert_pdf(path, type='text').decode()
+        text = convert_pdf(path, type='text')
         os.remove(path)
 
         days = re.split(r'(\w+day, \w+ \d{1,2})', text)
         date = None
-        for day in enumerate(days[1:]):
+        for day in enumerate(days[1: ]):
             if day[0] % 2 == 0:
                 # Calendar is put out for the current week, so use that year
                 date = day[1] + ", " + str(datetime.datetime.now().year)
@@ -109,7 +114,7 @@ class OHEventScraper(Scraper):
 
                 events = re.split(r'\n\n((?:\w+\s?)+),\s', day[1])
                 comm = ''
-                for event in enumerate(events[1:]):
+                for event in enumerate(events[1: ]):
                     if event[0] % 2 == 0:
                         comm = event[1].strip()
                     else:
@@ -138,33 +143,41 @@ class OHEventScraper(Scraper):
                         description = '\n'.join([
                                 x.strip() for x in
                                 description.split('\n')
-                                if x.strip() and
-                                not x.strip().startswith("Page ") and
-                                not x.strip().startswith("*Possible Vote") and
-                                not x.strip() == "NO OTHER COMMITTEES WILL MEET"
-                                ])
+                                if 
+                                        x.strip() and
+                                        not x.strip().startswith("Page ") and
+                                        not x.strip().startswith("*Possible Vote") and
+                                        not x.strip() == "NO OTHER COMMITTEES WILL MEET"
+                                ]).decode('ascii', 'ignore')
 
                         if not description:
                             description = '[No description provided by state]'
 
                         event = Event(
-                                name=description,
-                                start_time=time,
-                                location_name=location,
-                                timezone=self._tz.zone,
-                                description=description
-                        )
+                                session=session,
+                                when=time,
+                                type='committee:meeting',
+                                description=description,
+                                location=location
+                                )
 
                         event.add_source(PDF_URL)
-                        event.add_participant(comm, type='committee', note='host')
+                        event.add_participant(
+                                type='host',
+                                participant=comm,
+                                participant_type='committee',
+                                chamber='upper'
+                                )
                         for line in description.split('\n'):
-                            related_bill = re.search(r'(S\.?(?:[JC]\.?)?[BR]\.?\s+\d+)\s+(.*)$',
-                                                     line)
+                            related_bill = re.search(r'(S\.?(?:[JC]\.?)?[BR]\.?\s+\d+)\s+(.*)$', line)
                             if related_bill:
                                 (related_bill, relation) = related_bill.groups()
                                 relation = relation.strip()
                                 related_bill = related_bill.replace(".", "")
-                                item = event.add_agenda_item(relation)
-                                item.add_bill(related_bill)
+                                event.add_related_bill(
+                                        bill_id=related_bill,
+                                        type='consideration',
+                                        description=relation
+                                        )
 
-                        yield event
+                        self.save_event(event)

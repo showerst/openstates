15ab17ae718c3b4422d2525ce5d525a4c17b1d1c
Tim Showers <showerst@gmail.com>
2017-11-20 11:05:03 -0500

    AZ billy revert and add 2018 session

--- openstates/az/bills.py
+++ openstates/az/bills.py
@@ -1,87 +1,96 @@
+import re
 import json
 import datetime
 
-from pupa.scrape import Scraper, Bill
+from billy.scrape import NoDataForPeriod
+from billy.scrape.bills import BillScraper, Bill
+from billy.scrape.votes import Vote
 from . import utils
 from . import action_utils
-from . import session_metadata
 
 from lxml import html
-
+from lxml import etree
 
 BASE_URL = 'http://www.azleg.gov/'
 
+class AZBillScraper(BillScraper):
 
-class AZBillScraper(Scraper):
-    chamber_map = {'lower': 'H', 'upper': 'S'}
-    chamber_map_rev = {'H': 'upper', 'S': 'lower', 'G': 'executive', 'SS': 'executive'}
-    chamber_map_rev_eng = {'H': 'House', 'S': 'Senate', 'G': 'Governor',
-                           'SS': 'Secretary of State'}
+    """
+    Arizona Bill Scraper.
+    """
+    jurisdiction = 'az'
+    chamber_map = {'lower':'H', 'upper':'S'}
+    chamber_map_rev = {'H':'upper', 'S':'lower', 'G':'executive', 'SS':'executive'}
+    chamber_map_rev_eng = {'H':'House', 'S':'Senate', 'G':'Governor', 'SS':'Secretary of State'}
 
-    def scrape_bill(self, chamber, session, bill_id, session_id):
+    def get_session_id(self, session):
+        """
+        returns the session id for a given session
+        """
+        return self.metadata['session_details'][session]['session_id']
+
+    def scrape_bill(self, chamber, session, bill_id):
         """
         Scrapes documents, actions, vote counts and votes for
         a given bill.
         """
-        bill_json_url = 'https://apps.azleg.gov/api/Bill/?billNumber={}&sessionId={}&' \
-                        'legislativeBody={}'.format(bill_id, session_id, self.chamber_map[chamber])
+        session_id = self.get_session_id(session)
+        bill_json_url = 'https://apps.azleg.gov/api/Bill/?billNumber={}&sessionId={}&legislativeBody={}'.format(bill_id, session_id, self.chamber_map[chamber])
         response = self.get(bill_json_url)
-        # print(response.content)
-        page = json.loads(response.content.decode('utf-8'))
+        page = json.loads(response.content)
 
         bill_title = page['ShortTitle']
         bill_id = page['Number']
         internal_id = page['BillId']
         bill_type = self.get_bill_type(bill_id)
+
         bill = Bill(
-            bill_id,
-            legislative_session=session,
+            session=session,
             chamber=chamber,
+            bill_id=bill_id,
             title=bill_title,
-            classification=bill_type,
+            type=bill_type
         )
+        self.scrape_actions(bill, page)
+        self.scrape_versions(bill, internal_id)
+        self.scrape_sponsors(bill, internal_id)
+        self.scrape_subjects(bill, internal_id)
 
-        bill = self.scrape_actions(bill, page, chamber)
-        bill = self.scrape_versions(bill, internal_id)
-        bill = self.scrape_sponsors(bill, internal_id)
-        bill = self.scrape_subjects(bill, internal_id)
-
-        bill_url = 'https://apps.azleg.gov/BillStatus/BillOverview/{}?SessionId={}'.format(
-                    internal_id, session_id)
+        bill_url = 'https://apps.azleg.gov/BillStatus/BillOverview/{}?SessionId={}'.format(internal_id, session_id)
         bill.add_source(bill_url)
 
         bill = self.sort_bill_actions(bill)
 
-        yield bill
+        self.save_bill(bill)
+        #print json.dumps(page, indent=4)
 
     def scrape_versions(self, bill, internal_id):
         # Careful, this sends XML to a browser but JSON to machines
         # https://apps.azleg.gov/api/DocType/?billStatusId=68408
         versions_url = 'https://apps.azleg.gov/api/DocType/?billStatusId={}'.format(internal_id)
-        page = json.loads(self.get(versions_url).content.decode('utf-8'))
+        page = json.loads(self.get(versions_url).content)
         if page and 'Documents' in page[0]:
             for doc in page[0]['Documents']:
                 if doc['HtmlPath']:
-                    bill.add_version_link(
-                        note=doc['DocumentName'],
+                    bill.add_version(
+                        name=doc['DocumentName'],
                         url=doc['HtmlPath'],
-                        media_type='text/html'
+                        mimetype='text/html'
                     )
                 elif doc['PdfPath']:
-                    bill.add_version_link(
-                        note=doc['DocumentName'],
+                    bill.add_version(
+                        name=doc['DocumentName'],
                         url=doc['PdfPath'],
-                        media_type='application/pdf'
+                        mimetype='application/pdf'
                     )
                 else:
                     self.warning("No PDF or HTML version found for %s" % doc['DocumentName'])
-        return bill
 
     def scrape_sponsors(self, bill, internal_id):
         # Careful, this sends XML to a browser but JSON to machines
         # https://apps.azleg.gov/api/BillSponsor/?id=68398
         sponsors_url = 'https://apps.azleg.gov/api/BillSponsor/?id={}'.format(internal_id)
-        page = json.loads(self.get(sponsors_url).content.decode('utf-8'))
+        page = json.loads(self.get(sponsors_url).content)
         for sponsor in page:
             if 'Prime' in sponsor['SponsorType']:
                 sponsor_type = 'primary'
@@ -96,89 +105,66 @@ class AZBillScraper(Scraper):
                 sponsor['Legislator']['FirstName'],
                 sponsor['Legislator']['LastName'],
             )
-        bill.add_sponsorship(
-            classification=str(sponsor_type),
-            name=sponsor_name,
-            entity_type='person',
-            primary=sponsor_type == 'primary'
+
+        bill.add_sponsor(
+            type=sponsor_type,
+            name=sponsor_name
         )
-        return bill
 
     def scrape_subjects(self, bill, internal_id):
         # https://apps.azleg.gov/api/Keyword/?billStatusId=68149
         subjects_url = 'https://apps.azleg.gov/api/Keyword/?billStatusId={}'.format(internal_id)
-        page = json.loads(self.get(subjects_url).content.decode('utf-8'))
+        page = json.loads(self.get(subjects_url).content)
+        subjects = []
         for subject in page:
-            bill.add_subject(subject['Name'])
-        return bill
+            subjects.append(subject['Name'])
+        bill['subjects'] = subjects
 
-    def scrape_actions(self, bill, page, self_chamber):
+    def scrape_actions(self, bill, page):
         """
         Scrape the actions for a given bill
 
         AZ No longer provides a full list, just a series of keys and dates.
         So map that backwards using action_map
         """
-        for status in page['BillStatusAction']:
-            if status['Action'] in action_utils.status_action_map:
-                category = action_utils.status_action_map[status['Action']]
-                if status['Committee']['TypeName'] == 'Floor':
-                    categories = [category]
-                    if status['Committee']['CommitteeShortName'] == 'THIRD':
-                        categories.append('reading-3')
-                elif status['Committee']['TypeName'] == 'Standing':
-                    categories = ['committee-{}'.format(category)]
-                else:
-                    raise ValueError(
-                        'Unexpected committee type: {}'.format(status['Committee']['TypeName']))
-                action_date = datetime.datetime.strptime(
-                    status['ReportDate'], '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
-                bill.add_action(
-                    description=status['Action'],
-                    chamber={
-                        'S': 'upper',
-                        'H': 'lower',
-                    }[status['Committee']['LegislativeBody']],
-                    date=action_date,
-                    classification=categories,
-                )
         for action in action_utils.action_map:
             if page[action] and action_utils.action_map[action]['name'] != '':
                 try:
-                    action_date = datetime.datetime.strptime(
-                        page[action], '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d')
-
+                    action_date = datetime.datetime.strptime(page[action], '%Y-%m-%dT%H:%M:%S')
                     bill.add_action(
-                        chamber=self.actor_from_action(bill, action, self_chamber),
-                        description=action_utils.action_map[action]['name'],
+                        actor=self.actor_from_action(bill, action),
+                        action=action_utils.action_map[action]['name'],
                         date=action_date,
-                        classification=action_utils.action_map[action]['action'],
+                        type=action_utils.action_map[action]['action']
                     )
                 except ValueError:
-                    self.info("Invalid Action Time {} for {}".format(page[action], action))
+                    self.warning("Invalid Action Time {} for {}".format(page[action], action))
 
         # Governor Signs and Vetos get different treatment
         if page['GovernorAction'] == 'Signed':
-            action_date = page['GovernorActionDate'].split('T')[0]
+            action_date = datetime.datetime.strptime(page['GovernorActionDate'],
+                                                     '%Y-%m-%dT%H:%M:%S')
             bill.add_action(
-                chamber='executive',
-                description='Signed by Governor',
+                actor='executive',
+                action='Signed by Governor',
                 date=action_date,
-                classification='executive-signature',
+                type='governor:signed'
             )
 
         if page['GovernorAction'] == 'Vetoed':
-            action_date = page['GovernorActionDate'].split('T')[0]
+            action_date = datetime.datetime.strptime(page['GovernorActionDate'],
+                                                     '%Y-%m-%dT%H:%M:%S')
             bill.add_action(
-                chamber='executive',
-                description='Vetoed by Governor',
+                actor='executive',
+                action='Vetoed by Governor',
                 date=action_date,
-                classification='executive-veto',
+                type='governor:vetoed'
             )
 
         # Transmit to (X) has its own data structure as well
         for transmit in page['BodyTransmittedTo']:
-            action_date = transmit['TransmitDate'].split('T')[0]
+            action_date = datetime.datetime.strptime(transmit['TransmitDate'],
+                                                     '%Y-%m-%dT%H:%M:%S')
             # upper, lower, executive
             action_actor = self.chamber_map_rev[transmit['LegislativeBody']]
             # house, senate, governor
@@ -187,20 +173,20 @@ class AZBillScraper(Scraper):
             action_text = 'Transmit to {}'.format(body_text)
 
             if action_actor == 'executive':
-                action_type = 'executive-receipt'
+                action_type = 'governor:received'
             else:
-                action_type = None
+                action_type = 'other'
 
-            bill.add_action(
-                chamber=action_actor,
-                description=action_text,
-                date=action_date,
-                classification=action_type
-            )
+            if action_type == 'governor:received':
+                bill.add_action(
+                    actor=action_actor,
+                    action=action_text,
+                    date=action_date,
+                    type=action_type
+                )
 
-        return bill
 
-    def actor_from_action(self, bill, action, self_chamber):
+    def actor_from_action(self, bill, action):
         """
         Determine the actor from the action key
         If the action_map = 'chamber', return the bill's home chamber
@@ -209,17 +195,17 @@ class AZBillScraper(Scraper):
         for key in action_map:
             if key in action:
                 if action_map[key] == 'chamber':
-                    return self_chamber
+                    return bill['chamber']
                 else:
                     return action_map[key]
 
-    def scrape(self, chamber=None, session=None):
-        if not session:
-            session = self.latest_session()
-            self.info('no session specified, using %s', session)
-        session_id = session_metadata.session_id_meta_data[session]
+    def scrape(self, chamber, session):
+        try:
+            session_id = self.get_session_id(session)
+        except KeyError:
+            raise NoDataForPeriod(session)
 
-        # Get the bills page to start the session
+        #Get the bills page to start the session
         req = self.get('http://www.azleg.gov/bills/')
 
         session_form_url = 'http://www.azleg.gov/azlegwp/setsession.php'
@@ -233,31 +219,33 @@ class AZBillScraper(Scraper):
         page = self.get(bill_list_url, cookies=req.cookies).content
         # There's an errant close-comment that browsers handle
         # but LXML gets really confused.
-        page = page.replace(b'--!>', b'-->')
+        page = page.replace('--!>', '-->')
         page = html.fromstring(page)
 
         bill_rows = []
-        chambers = [chamber] if chamber else ['upper', 'lower']
-        for chamber in chambers:
-            if chamber == 'lower':
-                bill_rows = page.xpath('//div[@name="HBTable"]//tbody//tr')
-            else:
-                bill_rows = page.xpath('//div[@name="SBTable"]//tbody//tr')
+        if(chamber == 'lower'):
+            bill_rows = page.xpath('//div[@name="HBTable"]//tbody//tr')
             for row in bill_rows:
                 bill_id = row.xpath('td/a/text()')[0]
-                yield from self.scrape_bill(chamber, session, bill_id, session_id)
+                self.scrape_bill(chamber, session, bill_id)
 
-        # TODO: MBTable - Non-bill Misc Motions?
+        elif (chamber == 'upper'):
+            bill_rows = page.xpath('//div[@name="SBTable"]//tbody//tr')
+            for row in bill_rows:
+                bill_id = row.xpath('td/a/text()')[0]
+                self.scrape_bill(chamber, session, bill_id)
+
+        #TODO: MBTable - Non-bill Misc Motions?
 
     def sort_bill_actions(self, bill):
-        actions = bill.actions
+        actions = bill['actions']
         actions_list = []
         out_of_order = []
         new_list = []
         if not actions:
             return bill
         action_date = actions[0]['date']
-        actions[0]['description'] = actions[0]['description'].lower()
+        actions[0]['action'] = actions[0]['action'].lower()
         actions_list.append(actions[0])
         # seperate the actions that are out of order
         for action in actions[1:]:
@@ -266,8 +254,9 @@ class AZBillScraper(Scraper):
             else:
                 actions_list.append(action)
                 action_date = action['date']
-            action['description'] = action['description'].lower()
+            action['action'] = action['action'].lower()
         action_date = actions_list[0]['date']
+
 
         for action in actions_list:
             # this takes care of the actions in beween
@@ -286,14 +275,15 @@ class AZBillScraper(Scraper):
                     new_list.append(out_of_order.pop(o_index))
 
         if out_of_order != []:
-            self.info("Unable to sort " + bill.identifier)
+            self.log("Unable to sort " + bill['bill_id'])
             return bill
         else:
-            bill.actions = new_list
+            bill['actions'] = new_list
             return bill
 
     def get_bill_type(self, bill_id):
         for key in utils.bill_types:
             if key in bill_id.lower():
                 return utils.bill_types[key]
-        return None
+
+        return False

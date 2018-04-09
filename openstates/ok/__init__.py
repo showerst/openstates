b52a48bc065aa87e9e84097e75c23a515f9f955a
Tim Showers <showerst@gmail.com>
2018-04-09 11:20:32 -0500

    OK: git catchup

--- openstates/ok/__init__.py
+++ openstates/ok/__init__.py
@@ -32,7 +32,7 @@ metadata = dict(
          {'name': '2017-2018',
           'start_year': 2017,
           'end_year': 2018,
-          'sessions': ['2017-2018','2017SS1']},
+          'sessions': ['2017SS1','2017-2018','2017SS2','2018']},
         ],
     session_details={
         # On the Oklahoma website they list 2011/2012 as separate sessions, but
@@ -42,49 +42,78 @@ metadata = dict(
         '2011-2012':
             {'display_name': '2011-2012 Regular Session',
              'session_id': '1200',
-             '_scraped_name': '2012'
+             '_scraped_name': '2012 Regular Session'
             },
         '2012SS1':
             {'display_name': '2012 Special Session',
              'session_id': '121X',
-             '_scraped_name': '2012s'
+             '_scraped_name': '2012 Special Session'
             },
         '2013SS1':
             {'display_name': '2013 Special Session',
              'session_id': '131X',
-             '_scraped_name': '2013s',
+             '_scraped_name': '2013 Special Session',
             },
          '2013-2014':
             {'display_name': '2013-2014 Regular Session',
              'session_id': '1400',
-             '_scraped_name': '2014',
+             '_scraped_name': '2014 Regular Session',
             },
          '2015-2016':
             {'display_name': '2015-2016 Regular Session',
              'session_id': '1600',
-             '_scraped_name': '2016',
+             '_scraped_name': '2016 Regular Session',
             },
          '2017-2018':
             {'display_name': '2017-2018 Regular Session',
              'session_id': '1700',
-             '_scraped_name': '2017',
+             '_scraped_name': '2017 Regular Session',
             },
         '2017SS1':
             {'display_name': '2017 Special Session',
              'session_id': '171X',
-             '_scraped_name': '2017s'
-            },            
+             '_scraped_name': '2017 Special Session'
+            },
+        '2017SS2':
+            {'display_name': '2017 Second Special Session',
+             'session_id': '172X',
+             '_scraped_name': '2017 Second Special Session',
+            },
+        '2018':
+            {'display_name': '2018 Regular Session',
+             'session_id': '1800',
+             '_scraped_name': '2018 Regular Session',
+            },
         },
     feature_flags=['subjects', 'influenceexplorer'],
     _ignored_scraped_sessions=[
-u'2015', u'2013', u'2011s', u'2011', u'2010', u'2009', u'2008', u'2007', u'2006', u'2005', u'2005s', u'2004', u'2003', u'2003s', u'2002', u'2001', u'2001s', u'2000', u'2000s', u'1999', u'1998', u'1997', u'1997s']
+        '2017 First Special Session',
+        '2017 Second Special Session',
+        '2015 Regular Session',
+        '2013 Regular Session',
+        '2011 Regular Session', '2010 Regular Session',
+        '2009 Regular Session', '2008 Regular Session',
+        '2007 Regular Session',
+        '2006 Second Special Session',
+        '2006 Regular Session',
+        '2005 Special Session', '2005 Regular Session',
+        '2004 Special Session', '2004 Regular Session',
+        '2003 Regular Session', '2002 Regular Session',
+        '2001 Special Session', '2001 Regular Session',
+        '2000 Regular Session', '1999 Special Session',
+        '1999 Regular Session', '1998 Regular Session',
+        '1997 Regular Session', '1996 Regular Session',
+        '1995 Regular Session',
+        '1994 Second Special Session',
+        '1994 First Special Session',
+        '1994 Regular Session', '1993 Regular Session']
     )
 
 
 def session_list():
     from billy.scrape.utils import url_xpath
-    sessions = url_xpath('http://www.sdlegislature.gov/Legislative_Session/archive.aspx',
-                        '//table//td[@data-title="Year"]/text()')
+    sessions = url_xpath('http://webserver1.lsb.state.ok.us/WebApplication2/WebForm1.aspx',
+                        "//select[@name='cbxSession']/option/text()")
     # OK Sometimes appends (Mainsys) to their session listings
     sessions = [s.replace('(Mainsys)', '').strip() for s in sessions]
     return sessions
